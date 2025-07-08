865f804105e9d954b9546a4ef9818fd85ec04f70
Eduardo Jirón <emjironal@gmail.com>
2025-07-08 13:20:06 -0600

    add full website

---
+++ medical/precache-manifest.0017247416dd158db9cfe7aa08bdaa1b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cefcfd9bd1484849fcd16466d3689589",
+    "url": "./index.html"
+  },
+  {
+    "revision": "6709ac17ff38dea7047e",
+    "url": "./static/css/2.645d714c.chunk.css"
+  },
+  {
+    "revision": "4b8f60583a7922b17050",
+    "url": "./static/css/main.0631546d.chunk.css"
+  },
+  {
+    "revision": "6709ac17ff38dea7047e",
+    "url": "./static/js/2.9364dd89.chunk.js"
+  },
+  {
+    "revision": "bf47400037e37f38d5e1cbe7e2836531",
+    "url": "./static/js/2.9364dd89.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b8f60583a7922b17050",
+    "url": "./static/js/main.d18f711b.chunk.js"
+  },
+  {
+    "revision": "6f6ae7fc5ce8eab3fa94",
+    "url": "./static/js/runtime-main.91816c28.js"
+  }
+]);
